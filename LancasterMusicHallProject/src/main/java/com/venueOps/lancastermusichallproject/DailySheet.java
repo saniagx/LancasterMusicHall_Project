53a4b9c03a89c61673c2c4f7478e4c72c386d86e
neil <neil.daya@city.ac.uk>
2025-03-21 12:17:13 +0000

    implemented conceptual logic
    -purely conceptual, i need test data like current events to make sure it works
    -in theory, it should go through all events for the day and fill up the relevant venue's information correctly

--- LancasterMusicHallProject/src/main/java/com/venueOps/lancastermusichallproject/DailySheet.java
+++ LancasterMusicHallProject/src/main/java/com/venueOps/lancastermusichallproject/DailySheet.java
@@ -1,11 +1,15 @@
 package com.venueOps.lancastermusichallproject;
 
+import com.venueOps.lancastermusichallproject.operations.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class DailySheet {
 
@@ -51,10 +55,113 @@ public class DailySheet {
     @FXML Pane meetingRoom_pane;
     @FXML Text meetingPane_nameText;
 
+    private LocalDate date;
+    private ArrayList<Event> events;
+
     public DailySheet() {}
 
-    @FXML private void initialize() {
+    @FXML private void initialize() throws Exception {
+        disableByDefault();
         meetingRoom_pane.setVisible(false);
+        events = getEvents();
+        initialiseEvents();
+    }
+
+    public void setDate(LocalDate date) { this.date = date; }
+    public LocalDate getDate() { return date; }
+
+    // By default, assumes all venues are unused
+    public void disableByDefault() {
+        // Used texts
+        mainHall_usedText.setStrikethrough(true);
+        smallHall_usedText.setStrikethrough(true);
+        rehearsalSpace_usedText.setStrikethrough(true);
+        theGreenRoom_usedText.setStrikethrough(true);
+        bronteBoardroom_usedText.setStrikethrough(true);
+        dickensDen_usedText.setStrikethrough(true);
+        poeParlor_usedText.setStrikethrough(true);
+        globeRoom_usedText.setStrikethrough(true);
+        chekhovChamber_usedText.setStrikethrough(true);
+        // Grid panes
+        mainHall_grid.setDisable(true);
+        smallHall_grid.setDisable(true);
+        rehearsalSpace_grid.setDisable(true);
+        // Expand detail buttons
+        theGreenRoom_button.setDisable(true);
+        bronteBoardroom_button.setDisable(true);
+        dickensDen_button.setDisable(true);
+        poeParlor_button.setDisable(true);
+        globeRoom_button.setDisable(true);
+        chekhovChamber_button.setDisable(true);
+    }
+
+    public ArrayList<Event> getEvents() {
+        // Fetch all events for specific day
+        return new ArrayList<>();
+    }
+
+    // Iterates through events and fills in information for each venue
+    public void initialiseEvents() throws Exception {
+        for (Event event : events) {
+            switch (event.getVenueID()) {
+                case 0: // Main Hall
+                    mainHall_usedText.setStrikethrough(false);
+                    mainHall_grid.setDisable(false);
+                    mainHall_who.setText(event.getEventHost());
+                    mainHall_startTime.setText(event.getEventStart().toString());
+                    mainHall_endTime.setText(event.getEventEnd().toString());
+                    mainHall_seatingConfig.setText(String.valueOf(event.getSeatingConfigID())); // This should probably be replaced with the name of a seating config
+                    break;
+                case 1: // Small Hall
+                    smallHall_usedText.setStrikethrough(false);
+                    smallHall_grid.setDisable(false);
+                    smallHall_who.setText(event.getEventHost());
+                    smallHall_startTime.setText(event.getEventStart().toString());
+                    smallHall_endTime.setText(event.getEventEnd().toString());
+                    smallHall_seatingConfig.setText(String.valueOf(event.getSeatingConfigID()));
+                    break;
+                case 2: // Rehearsal Space
+                    rehearsalSpace_usedText.setStrikethrough(false);
+                    rehearsalSpace_grid.setDisable(false);
+                    rehearsalSpace_who.setText(event.getEventHost());
+                    rehearsalSpace_startTime.setText(event.getEventStart().toString());
+                    rehearsalSpace_endTime.setText(event.getEventEnd().toString());
+                    rehearsalSpace_seatingConfig.setText(String.valueOf(event.getSeatingConfigID()));
+                    break;
+                case 3: // The Green Room
+                    theGreenRoom_usedText.setStrikethrough(false);
+                    theGreenRoom_button.setDisable(false);
+                    // LOGIC TO FILL EXPAND DETAILS WITH MEETING EVENTS
+                    break;
+                case 4: // Bronte Boardroom
+                    bronteBoardroom_usedText.setStrikethrough(false);
+                    bronteBoardroom_button.setDisable(false);
+                    // LOGIC TO FILL EXPAND DETAILS WITH MEETING EVENTS
+                    break;
+                case 5: // Dickens Den
+                    dickensDen_usedText.setStrikethrough(false);
+                    dickensDen_button.setDisable(false);
+                    // LOGIC TO FILL EXPAND DETAILS WITH MEETING EVENTS
+                    break;
+                case 6: // Poe Parlor
+                    poeParlor_usedText.setStrikethrough(false);
+                    poeParlor_button.setDisable(false);
+                    // LOGIC TO FILL EXPAND DETAILS WITH MEETING EVENTS
+                    break;
+                case 7: // Globe Room
+                    globeRoom_usedText.setStrikethrough(false);
+                    globeRoom_button.setDisable(false);
+                    // LOGIC TO FILL EXPAND DETAILS WITH MEETING EVENTS
+                    break;
+                case 8: // Chekhov Chamber
+                    chekhovChamber_usedText.setStrikethrough(false);
+                    chekhovChamber_button.setDisable(false);
+                    // LOGIC TO FILL EXPAND DETAILS WITH MEETING EVENTS
+                    break;
+                default:
+                    throw new Exception("Invalid event ID for " + event.getEventName());
+            }
+        }
     }
 
     public void BackButton() {
@@ -64,8 +171,7 @@ public class DailySheet {
     public void OpenMeetingRoomPane(String venue) {
         meetingRoom_pane.setVisible(true);
         meetingPane_nameText.setText(venue);
-        // Needs logic to fetch all meeting events for the day
-        // For each event, add a new grid to the pane containing details like Who, StartTime, EndTime
+        // For each meeting event, add a new grid to the pane containing details like Who, StartTime, EndTime
         // (no SeatingConfig needed as meeting rooms don't have seating configs)
     }
 
